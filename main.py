31a76e721a58d4fa96deae4b6a86d9da5318a292
sghan811 <59792000+sghan811@users.noreply.github.com>
2021-08-08 14:48:48 +0900

    project0 making

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import os, sys
 currDir = os.path.dirname(os.path.realpath(__file__))
 rootDir = os.path.abspath(os.path.join(currDir, '..'))
--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
-import os
+import io
+from setuptools import find_packages, setup
 
-os.system('pip install art')
+
+# Read in the README for the long description on PyPI
+def long_description():
+    with io.open('README.rst', 'r', encoding='utf-8') as f:
+        readme = f.read()
+    return readme
+
+setup(name='project0',
+      version='0.1',
+      description='project0',
+      long_description=long_description(),
+      url='https://github.com/sghan811/project0',
+      author='sghan811',
+      author_email='sghan811@gmail.com',
+      license='DASTY',
+      packages=find_packages(),
+      classifiers=[
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          ],
+      zip_safe=False)
